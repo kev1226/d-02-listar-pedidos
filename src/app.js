0926f36ab4b0e88722a81be9c2c51cb36c1ace4c
kev1226 <kevin_alt12@hotmail.com>
2025-05-19 00:39:02 -0500

    deploy front

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const path = require('path'); // Necesario para servir HTML
 
 const pedidoRoutes = require('./routes/pedido.routes');
 
 app.use(express.json());
 app.use('/api/pedidos', pedidoRoutes);
+
+
+// Servir archivos estáticos (como HTML) desde la carpeta "public"
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado a MongoDB Atlas'))
